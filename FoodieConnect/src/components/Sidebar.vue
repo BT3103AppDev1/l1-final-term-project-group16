5fe2a7bdd026d98829ba35f306193a9392c74d9b
linggweii <119219522+linggweii@users.noreply.github.com>
2024-04-10 12:33:16 +0800

    modified:   FoodieConnect/src/components/Sidebar.vue

--- FoodieConnect/src/components/Sidebar.vue
+++ FoodieConnect/src/components/Sidebar.vue
@@ -3,7 +3,7 @@
         <RouterLink to="/homepage" class="menu-item">Home</RouterLink>
         <RouterLink to="/profile" class="menu-item">Profile</RouterLink>
         <RouterLink to="/food-diary" class="menu-item">Food Diary</RouterLink>
-        <RouterLink to="/new-post" class="menu-item">New Post</RouterLink>
+        <RouterLink to="/postpage" class="menu-item">New Post</RouterLink>
         <RouterLink to="/saved-list" class="menu-item">Saved List</RouterLink>
         <RouterLink to="/food-near-me" class="menu-item">Food Near Me</RouterLink>
         <RouterLink to="/communities" class="menu-item">Communities</RouterLink>
